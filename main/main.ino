8917faf96c10d930e34f7c3ef487dd59fbb4617a
jenoh <memint.contact@gmail.com>
2022-07-15 14:18:35 +0200

    init

---
+++ main/main.ino
@@ -0,0 +1,134 @@
+#include <Wire.h>
+#include <rpcWiFi.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+const char* ssid = "";
+const char* password =  "";
+
+void setup() 
+{
+  Serial.begin(115200);
+      while(!Serial); // Wait for Serial to be ready
+
+    initWifi();
+ 
+  // Initialize MPU6050
+  Serial.println("Initialize MPU6050");
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(1000);
+  }
+  
+  mpu.calibrateGyro();
+
+  mpu.setThreshold(3);
+  
+  checkSettings();
+}
+
+void checkSettings()
+{
+  Serial.println();
+  
+  Serial.print(" * Sleep Mode:        ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+  
+  Serial.print(" * Clock Source:      ");
+  switch(mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  
+  Serial.print(" * Gyroscope:         ");
+  switch(mpu.getScale())
+  {
+    case MPU6050_SCALE_2000DPS:        Serial.println("2000 dps"); break;
+    case MPU6050_SCALE_1000DPS:        Serial.println("1000 dps"); break;
+    case MPU6050_SCALE_500DPS:         Serial.println("500 dps"); break;
+    case MPU6050_SCALE_250DPS:         Serial.println("250 dps"); break;
+  } 
+  
+  Serial.print(" * Gyroscope offsets: ");
+  Serial.print(mpu.getGyroOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getGyroOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getGyroOffsetZ());
+  
+  Serial.println();
+}
+
+void initWifi()
+{
+ 
+ // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+ 
+    Serial.println("Connecting to WiFi..");
+    WiFi.begin(ssid, password);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+        WiFi.begin(ssid, password);
+    }
+    Serial.println("Connected to the WiFi network");
+    Serial.print("IP Address: ");
+    Serial.println (WiFi.localIP()); // prints out the device's IP address 
+}
+
+void scanWifi() 
+{
+            // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+            delay(10);
+        }
+    }
+    Serial.println("");
+}
+void loop()
+{
+  Vector rawGyro = mpu.readRawGyro();
+  Vector normGyro = mpu.readNormalizeGyro();
+
+  Serial.print(" Xraw = ");
+  Serial.print(rawGyro.XAxis);
+  Serial.print(" Yraw = ");
+  Serial.print(rawGyro.YAxis);
+  Serial.print(" Zraw = ");
+  Serial.println(rawGyro.ZAxis);
+
+  Serial.print(" Xnorm = ");
+  Serial.print(normGyro.XAxis);
+  Serial.print(" Ynorm = ");
+  Serial.print(normGyro.YAxis);
+  Serial.print(" Znorm = ");
+  Serial.println(normGyro.ZAxis);
+  
+  delay(1000);
+}
